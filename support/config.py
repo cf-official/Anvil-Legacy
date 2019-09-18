77e04dfb6084233fa0dddea8a282dc21cecbe3ae
LunarLite <mdtozer@hotmail.com>
2019-09-18 11:37:47 +0200

    Delete config.py

--- support/config.py
+++
@@ -1,6 +0,0 @@
-# Needs to be properly hidden from repo mmyes.
-
-class Config:
-    embed_footer = "https://discord.gg/EYKxkce"
-    token = "NjA3MTYzNDI0MzYyODU2NDU4.XWe0RQ.au6xGWcZbaKjgKxwogWQ8i58bhI"
-    default_prefix = "."
